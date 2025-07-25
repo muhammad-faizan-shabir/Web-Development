e040275306ae52467937524fe26b3d7f20ee9907
faizanshabir123 <faizanshabir69@gmail.com>
2025-07-25 17:53:26 +0500

    Update main.js

--- Traversy Media Youtube Channel/JavaScript DOM Crash Course/Project/main.js
+++ Traversy Media Youtube Channel/JavaScript DOM Crash Course/Project/main.js
@@ -14,69 +14,69 @@ filter.addEventListener('keyup', filterItems);
 // Add item
 function addItem(e)
 {
-    e.preventDefault();
+	e.preventDefault();
 
     // Get input value
-    let newItem = document.getElementById('item').value;
+	let newItem = document.getElementById('item').value;
 
     // Create new li element
-    let li = document.createElement('li');
+	let li = document.createElement('li');
 
     // Add class
-    li.className = 'list-group-item';
+	li.className = 'list-group-item';
 
     // Add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+	li.appendChild(document.createTextNode(newItem));
 
     // Create del button element
-    let deleteBtn = document.createElement('button');
+	let deleteBtn = document.createElement('button');
 
     // Add classes to del button
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+	deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
     // Append text node
-    deleteBtn.appendChild(document.createTextNode('X'));
+	deleteBtn.appendChild(document.createTextNode('X'));
 
     // Append button to li
-    li.appendChild(deleteBtn);
+	li.appendChild(deleteBtn);
 
     // Append li to list
-    itemList.appendChild(li);
+	itemList.appendChild(li);
 }
 
 // Remove item
 function removeItem(e)
 {
-    if(e.target.classList.contains('delete'))
-    {
-        if(confirm('Are You Sure?'))
-        {
-            let li = e.target.parentElement;
-            itemList.removeChild(li);
-        }
-    }
+	if(e.target.classList.contains('delete'))
+	{
+		if(confirm('Are You Sure?'))
+		{
+			let li = e.target.parentElement;
+			itemList.removeChild(li);
+		}
+	}
 }
 
 // Filter Items
 function filterItems(e)
 {
     // convert text to lowercase
-    let text = e.target.value.toLowerCase();
+	let text = e.target.value.toLowerCase();
 
     // Get lis
-    let items = itemList.getElementsByTagName('li');
+	let items = itemList.getElementsByTagName('li');
 
     // Convert to an array
-    Array.from(items).forEach(function(item)
-    {
-        let itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1)
-        {
-            item.style.display = 'block';
-        } 
-        else 
-        {
-            item.style.display = 'none';
-        }
-    });
+	Array.from(items).forEach(function(item)
+	{
+		let itemName = item.firstChild.textContent;
+		if(itemName.toLowerCase().indexOf(text) != -1)
+		{
+			item.style.display = 'block';
+		} 
+		else 
+		{
+			item.style.display = 'none';
+		}
+	});
 }
